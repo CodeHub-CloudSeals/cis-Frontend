47147460ff21d8ed6d94a67ffe58b51c9f9b8975
CodeHub-CloudSeals <codehub@cloudseals.com>
2025-07-23 21:15:50 +0530

    Added apiroutes for refactoring

---
+++ src/Constants/apiRoutes.js
@@ -0,0 +1,11 @@
+// src/constants/apiRoutes.js
+
+export const API_ROUTES = {
+  REGISTER: '/cloudseal/v1/api/register',
+  LOGIN: '/cloudseal/v1/api/login',
+  USER: '/cloudseal/v1/api/user',
+  ORGANIZATIONS: '/cloudseal/v1/api/organizations',
+  AI_AGENT: '/cloudseal/v1/api/aiagent',
+  TENANT_AGENT: '/cloudseal/v1/api/tenantagent',
+  AGENT_CATALOG: '/cloudseal/v1/api/agentcatalog',
+};
