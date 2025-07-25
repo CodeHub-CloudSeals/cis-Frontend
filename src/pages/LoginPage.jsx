994dc35764de28a28efc5fa443472206b6f72766
CodeHub-CloudSeals <codehub@cloudseals.com>
2025-07-25 17:46:34 +0530

    Commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -13,6 +13,7 @@ import {
 } from "react-icons/fa";
 import { MdMail } from "react-icons/md";
 import axios from "axios";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log("API url", API_BASE_URL);
@@ -25,6 +26,16 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [phone, setPhone] = useState("");
+  const [orgName, setOrgName] = useState("");
+  const [domain, setDomain] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [companyAddress, setCompanyAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [stateName, setStateName] = useState("");
+  const [zipCode, setZipCode] = useState("");
 
   const clearMessage = () => {
     setTimeout(() => setMessage(""), 3000);
@@ -54,8 +65,6 @@ export default function LoginPage() {
       if (token) {
         console.log("Saving token:", token);
         localStorage.setItem("token", token);
-        console.log("Stored token:", localStorage.getItem("token"));
-
         setTimeout(() => {
           setMessage("Login successful!");
           clearMessage();
@@ -82,16 +91,29 @@ export default function LoginPage() {
       clearMessage();
       return;
     }
+
     setMessage("Signing up...");
     try {
-      const res = await axios.post(`${API_BASE_URL}${API_ROUTES.REGISTER}`, {
-        username,
-        email,
-        password,
-        role: "admin",
-        status: "active",
-        organizations: { id: 2 },
-      });
+      const res = await axios.post(
+        "http://34.45.198.251:9092/cloudseal/v1/api/register",
+        {
+          username,
+          email,
+          password,
+          phone,
+          role: "admin",
+          status: "Active",
+          organizations: {
+            name: orgName,
+            domain,
+            companyName,
+            companyAddress,
+            country,
+            state: stateName,
+            zipCode,
+          },
+        }
+      );
 
       setMessage("Signup successful!");
       clearMessage();
@@ -150,47 +172,150 @@ export default function LoginPage() {
               <input
                 type="text"
                 placeholder="Username"
-                className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 
-              focus:outline-none focus:ring-2 focus:ring-[#3c2958] 
+                className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
               hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
               />
+
               {isSignUp && (
-                <input
-                  type="email"
-                  placeholder="Email Address"
-                  className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 
-              focus:outline-none focus:ring-2 focus:ring-[#3c2958] 
+                <>
+                  <input
+                    type="email"
+                    placeholder="Email Address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
               hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+                    required
+                  />
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Create Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 focus:outline-none focus:ring-2 focus:ring-[#3c2958] hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                      required
+                    />
+                    <div
+                      className="absolute right-3 top-3 cursor-pointer text-gray-400"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </div>
+                  </div>
+
+                  <div className="relative">
+                    <input
+                      type={showConfirmPassword ? "text" : "password"}
+                      placeholder="Confirm Password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 focus:outline-none focus:ring-2 focus:ring-[#3c2958] hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                      required
+                    />
+                    <div
+                      className="absolute right-3 top-3 cursor-pointer text-gray-400"
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    >
+                      {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                    </div>
+                  </div>
+
+                  <input
+                    type="tel"
+                    placeholder="Phone Number"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Organization Name"
+                    value={orgName}
+                    onChange={(e) => setOrgName(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Domain"
+                    value={domain}
+                    onChange={(e) => setDomain(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Company Name"
+                    value={companyName}
+                    onChange={(e) => setCompanyName(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Company Address"
+                    value={companyAddress}
+                    onChange={(e) => setCompanyAddress(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Country"
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="State"
+                    value={stateName}
+                    onChange={(e) => setStateName(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Zip Code"
+                    value={zipCode}
+                    onChange={(e) => setZipCode(e.target.value)}
+                    className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
+              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
+                  />
+                </>
               )}
-              <input
-                type="password"
-                placeholder={isSignUp ? "Create Password" : "Password"}
-                className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 
-              focus:outline-none focus:ring-2 focus:ring-[#3c2958] 
-              hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              {isSignUp && (
+
+              {!isSignUp && (
                 <input
                   type="password"
-                  placeholder="Confirm Password"
-                  className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300 
-              focus:outline-none focus:ring-2 focus:ring-[#3c2958] 
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full px-4 py-2 bg-[#0a0b20] bg-opacity-20 text-gray-300
+              focus:outline-none focus:ring-2 focus:ring-[#3c2958]
               hover:ring-2 hover:ring-[#3c2958] transition duration-200 font-light font-['Open_Sans']"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
                   required
                 />
               )}
+
               <button
                 type="submit"
                 className="w-full bg-[#552ea2] hover:bg-[#3166e8] text-white py-2 rounded-2xl"
@@ -198,6 +323,7 @@ export default function LoginPage() {
                 {isSignUp ? "Sign Up" : "Login"}
               </button>
             </form>
+
             <p className="text-sm text-center mt-4 text-white uppercase font-light font-['Open_Sans']">
               {isSignUp
                 ? "ALREADY HAVE AN ACCOUNT ?"
@@ -214,18 +340,13 @@ export default function LoginPage() {
       </div>
 
       <div className="relative z-10 w-full bg-black bg-opacity-80 py-4">
-        {" "}
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-sm text-center text-gray-300">
           <div className="flex justify-center space-x-4 mb-5">
             {[FaFacebookF, FaLinkedinIn, FaTwitter, FaYoutube, FaInstagram].map(
               (Icon, idx) => (
                 <div
                   key={idx}
-                  className="cursor-pointer w-10 h-10 flex items-center justify-center
-                      rounded-full border border-opacity-90 border-[#3c2958]
-                      bg-black bg-opacity-80 text-white
-                      transition-transform duration-300 ease-in-out
-                      hover:scale-110 hover:bg-[#3166e8]"
+                  className="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border border-opacity-90 border-[#3c2958] bg-black bg-opacity-80 text-white transition-transform duration-300 ease-in-out hover:scale-110 hover:bg-[#3166e8]"
                 >
                   <Icon className="text-md" />
                 </div>
